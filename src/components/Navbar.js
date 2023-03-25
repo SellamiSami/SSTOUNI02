6031ef093d1623d75d040a9bfa220fcde1f96656
ahmed trabelsi <trabelsi.ahmed.pro@gmail.com>
2023-03-25 22:56:54 +0100

    fixed home image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import logo from '../images/logo.png'
-import { FaBars} from "react-icons/fa"
-import {Link} from 'react-router-dom'
-import {NavLink} from 'react-router-dom'
+import { FaBars } from "react-icons/fa"
+import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 
 
 export default class Navbar extends Component {
-    state={
-        isOpen:false
+    state = {
+        isOpen: false
     }
-    handleToggle = () =>{
-        this.setState({isOpen: !this.state.isOpen})
+    handleToggle = () => {
+        this.setState({ isOpen: !this.state.isOpen })
     }
     render() {
         return (
@@ -20,13 +20,13 @@ export default class Navbar extends Component {
                 <div className="nav-center">
                     <div className="nav-header">
                         <Link to="/">
-                            <img src={logo} alt="Ezar Company" className="logo-header"  />
+                            <h1 className='hotel-navbar'>Hotel Donia </h1>
                         </Link>
                         <button type="button" className="nav-btn" onClick={this.handleToggle}>
-                            <FaBars className="nav-icon"/>
+                            <FaBars className="nav-icon" />
                         </button>
-                    </div> 
-                    <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
+                    </div>
+                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
                         <li>
                             <NavLink exact to="/" activeClassName="active" >Home</NavLink>
                         </li>
@@ -44,7 +44,7 @@ export default class Navbar extends Component {
                         </li>
 
                     </ul>
-                </div> 
+                </div>
             </nav>
         )
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ import cars4 from '../images/carousel/4.png'
 import cars5 from '../images/carousel/5.jpg'
 import cars6 from '../images/carousel/6.png'
 import cars7 from '../images/carousel/7.png'
-import pic_home from '../images/3.jpg'
+
 
 
 
@@ -26,110 +26,110 @@ export default function Home() {
     return (
         <div className="home">
             <div className="social-home">
-            <img src={pic_home} className="pic-header pic-header-home" alt="pic_home" />
-                    <ul className="social-h">
-                            <a href={'https://www.facebook.com/ezar.co.ltd/'}>
-                        <li><FaFacebookSquare className="nav-icon social-media-h"/></li>
-                            </a>
-                            <a href={'https://www.linkedin.com/company/ezar-co-ltd//'}>
-                        <li><FaLinkedin className="nav-icon social-media-h"/></li>
-                            </a>
-                            <a href={'https://www.youtube.com/channel/UC8Zmeunw75u8tZycgo8TgiQ'}>
-                        <li><FaYoutubeSquare className="nav-icon social-media-h"/></li>
-                            </a>
-                            <a href={'https://www.youtube.com/channel/UC8Zmeunw75u8tZycgo8TgiQ'}>
-                        <li><FaEnvelope className="nav-icon social-media-h"/></li>
-                            </a>
-                    </ul>
+                < img src="./reception.jpg " className="cover-banner" alt="image" />
+                <ul className="social-h">
+                    <a href={'https://www.facebook.com/ezar.co.ltd/'}>
+                        <li><FaFacebookSquare className="nav-icon social-media-h" /></li>
+                    </a>
+                    <a href={'https://www.linkedin.com/company/ezar-co-ltd//'}>
+                        <li><FaLinkedin className="nav-icon social-media-h" /></li>
+                    </a>
+                    <a href={'https://www.youtube.com/channel/UC8Zmeunw75u8tZycgo8TgiQ'}>
+                        <li><FaYoutubeSquare className="nav-icon social-media-h" /></li>
+                    </a>
+                    <a href={'https://www.youtube.com/channel/UC8Zmeunw75u8tZycgo8TgiQ'}>
+                        <li><FaEnvelope className="nav-icon social-media-h" /></li>
+                    </a>
+                </ul>
 
-                </div>
+            </div>
             <div className="text-home">
-                <hr className="hr-home"/>
-                <p><FaQuoteLeft className="comma0"/></p>
+                <hr className="hr-home" />
+                <p><FaQuoteLeft className="comma0" /></p>
                 <p className="text-h" >
                     <span className="home-bold">W</span>e offer the full range of integrated services for EPC plants including: project management, process engineering, multi-disciplinary detailed engineering, procurement, industrial construction, commissioning and start-up. We also provide maintenance engineering and global service to increase your facilities performances.
                 </p>
-                <br/>
+                <br />
                 <p className="text-h" >
                     <span className="home-bold">W</span>e are particularly conversant with expertise in multidisciplinary liable to respect the project’s confidentiality with a mental of new business generation away of any direct or indirect corruption. We put on disposal a network of legal joint-ventured partners and suppliers with continuous assistance along project exaction. We deliver our services with an assurance of reasonable costs, reliable scheduling, and exceptional quality.
                 </p>
-                <br/>
+                <br />
                 <p className="text-h" >
                     <span className="home-bold">T</span>o meet the demands of strict budgets and tight deadlines, we have built teams of highly skilled international and local professionals and integrated the technology of today to exceed your expectations. We have the experience and manpower to complete your needs in a timely and cost-effective manner.
                 </p>
 
-                <p><FaQuoteRight className="comma1"/></p>
-                <br/>
-                <br/>
-                <br/>
+                <p><FaQuoteRight className="comma1" /></p>
+                <br />
+                <br />
+                <br />
 
-                <hr className="hr-home"/>
+                <hr className="hr-home" />
             </div>
             <div className="carrousel-home">
                 <div className="partner">
-                    <hr className="hr-home-partner"/>
-                        <p  className="text-partner" style={{fontSize:'24px' , margin:'0 5px'}}>OUR PARTNERS</p>
-                    <hr className="hr-home-partner"/>
+                    <hr className="hr-home-partner" />
+                    <p className="text-partner" style={{ fontSize: '24px', margin: '0 5px' }}>OUR PARTNERS</p>
+                    <hr className="hr-home-partner" />
                 </div>
-            <br/>
-            <hr className="hr-home-carousel"/>
+                <br />
+                <hr className="hr-home-carousel" />
 
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={cars1}
-                    alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={cars2}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={cars3}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={cars4}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={cars5}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={cars6}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={cars7}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cars1}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cars2}
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cars3}
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cars4}
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cars5}
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cars6}
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cars7}
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
                 </Carousel>
 
 
 
 
-            <hr className="hr-home-carousel"/>
+                <hr className="hr-home-carousel" />
 
 
             </div>
